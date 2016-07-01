af4dcec03ac8ba475f18fc91460bdc6363b8adc4
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-07-01 10:49:18 -0700

    refactored code that allows user to submit items

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-
-console.log('hey there');
 //LIST-MENU EVENTS
 
 	//User hovers over down list header
@@ -16,39 +14,37 @@ console.log('hey there');
 
 //LIST-MGMT-BOX EVENTS
 
-	//User submits input box with 'Enter'
-		$('html').keydown(function(event) {
-			var userInput = $('.add-item-text').val();
-			var listItemHtml = '<li> <div class="check-box">' +
-				'<i class="hidden fa fa-check-square-o" aria-hidden="true"></i>' +
-				'<i class="fa fa-square-o" aria-hidden="true"></i>' +
-				'</div><p>' +  
-				userInput +
-				'</p><i class="remove-button fa fa-trash-o" aria-hidden="true">' +
-				'</i></li>'
+	function add() {
+		var userInput = $('.add-item-text').val();
 
-			if (event.which == 13 && userInput != '') {
-				$('.current-list').prepend(listItemHtml);
-				$('.add-item-text').val('')
-			}
-		});
+		if (userInput.length > 0) {
+			var listItemHtml = '<li> <div class="check-box">' +
+			'<i class="hidden fa fa-check-square-o" aria-hidden="true"></i>' +
+			'<i class="fa fa-square-o" aria-hidden="true"></i>' +
+			'</div><p>' +  
+			userInput +
+			'</p><i class="remove-button fa fa-trash-o" aria-hidden="true">' +
+			'</i></li>'
+		} else {
+			alert('Please enter an item!')
+		};
+
+		return listItemHtml;
+	}
+
+	//User submits input box with 'Enter'
+	$('html').keydown(function(event) {
+		if (event.which == 13) {
+			$('.current-list').prepend(add());
+			$('.add-item-text').val('')
+		}
+	});
 
 	//User submits input box with Button
-		$('.add-button').click(function(event) {
-			var userInput = $('.add-item-text').val();
-			var listItemHtml = '<li> <div class="check-box">' +
-				'<i class="hidden fa fa-check-square-o" aria-hidden="true"></i>' +
-				'<i class="fa fa-square-o" aria-hidden="true"></i>' +
-				'</div><p>' +  
-				userInput +
-				'</p><i class="remove-button fa fa-trash-o" aria-hidden="true">' +
-				'</i></li>'
-
-			if (userInput != '') {
-				$('.current-list').prepend(listItemHtml);
-				$('.add-item-text').val('')
-			}
-		});
+	$('.add-button').click(function() {
+		$('.current-list').prepend(add());
+		$('.add-item-text').val('');
+	});
 
 	//User hovers over Check-all button
 	//User clicks Check-all button
@@ -59,20 +55,17 @@ console.log('hey there');
 //SHOPPING-LIST EVENTS
 
 	//User hovers over item
-
 	$('.shopping-list').on('mouseenter mouseleave', 'li', function() {
 		$(this).toggleClass('user-hover');
 	});
 
 	//User clicks check box
-
 	$('.shopping-list').on('click', '.check-box', function() {
 		$(this).children().toggleClass('hidden');
 		$(this).parent().toggleClass('checked');
 	});
 
 	//User hovers over remove-button
-
 	$('.shopping-list').on('mouseenter mouseleave', '.remove-button', function() {
 		$(this).toggleClass('icon-hover');
 	});
@@ -80,6 +73,6 @@ console.log('hey there');
 	//User clicks remove-button
 	$('.shopping-list').on('click', '.remove-button', function() {
 		$(this).parent().remove();
-	})
+	});
 
 })
